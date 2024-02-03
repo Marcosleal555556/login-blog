72376c383e7af08c4b6a1e7834ae6a2ed576ae80
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-02-03 13:29:31 -0300

    .

--- sql/mydb.sql
+++ sql/mydb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 25-Nov-2023 às 13:56
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.1.12
+-- Host: localhost:3306
+-- Tempo de geração: 03/02/2024 às 09:04
+-- Versão do servidor: 8.0.36-0ubuntu0.22.04.1
+-- Versão do PHP: 8.1.2-1ubuntu2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,38 +20,71 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `mydb`
 --
-CREATE DATABASE IF NOT EXISTS mydb;
+
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `users`
+-- Estrutura para tabela `Postagens`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `Postagens` (
+  `id` int NOT NULL,
+  `titulo` text NOT NULL,
+  `conteudo` text NOT NULL,
+  `autor` text NOT NULL,
+  `datapostagem` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabela de usuários';
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'marcos', 'dfadc855249b015fd2bb015c0b099b2189c58748');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `users`
+-- Índices de tabela `Postagens`
+--
+ALTER TABLE `Postagens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `users`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `Postagens`
+--
+ALTER TABLE `Postagens`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
